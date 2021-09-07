f212e9226a1bf102e46baa9b7b9b173576e40ad4
Ishtehar Hussain <ishtehar@alley.co>
2021-09-07 00:33:25 -0400

    Installed InertiaJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,21 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+
+createInertiaApp({
+
+    id: 'app',
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
+
+
+
+
