7a2d0a64ba8cc45e5a2d7c5fb240bdd3f3754824
Ishtehar Hussain <ishtehar@alley.co>
2021-09-07 00:35:52 -0400

    remove default param  --- id

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,6 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
 createInertiaApp({
-
-    id: 'app',
     resolve: name => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
