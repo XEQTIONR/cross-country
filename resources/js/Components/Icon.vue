97f585541b350e328d2e5ea616737e7f39a87411
Ishtehar Hussain <ishtehar@alley.co>
2021-09-09 20:00:02 -0400

    Front end scaffolding and Icon and Navigation components

---
+++ resources/js/Components/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <span v-html="require(`!!html-loader!../../svg/${icon}.svg`).default"></span>
+</template>
+
+<script>
+export default {
+ props: {
+     icon: {
+         type: String,
+         default: null,
+     }
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="h-screen bg-yellow-300 w-1/4">
+        <ul>
+            <li
+                class="flex flex-row justify-start items-center p-3.5"
+                v-for="item in listItems"
+            >
+                <icon
+                    class="mr-2"
+                    :icon="item.icon"
+                />
+                {{item.title}}
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Icon from "@/Components/Icon";
+export default {
+    components: {Icon},
+    props: {
+        items : {
+            type : Array,
+            default : []
+        }
+    },
+    data() {
+        return {
+            listItems : [
+                { title: 'Letters of Credit', icon: 'anchor' },
+                { title: 'Consignments', icon: 'anchor' },
+                { title: 'Orders', icon: 'box' },
+                { title: 'Payments', icon: 'cash' },
+                { title: 'Products', icon: 'disc' },
+                { title: 'Customers', icon: 'store' },
+                { title: 'Reports', icon: 'container' },
+                { title: 'Users', icon: 'cash' },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
