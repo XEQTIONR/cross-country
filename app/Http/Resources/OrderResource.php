5284c0f87264b2fda9d3a069be0f6028d725c76a
Ishtehar Hussain <ishtehar@alley.co>
2021-09-14 22:49:52 -0400

    orders.index

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,36 +14,50 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
+        $subTotal = is_numeric($this->subTotal)
+            ? floatval($this->subTotal)
+            : $this->contents->reduce(function($carry, $item) {
+                return $carry + (intval($item->qty) * floatval($item->unit_price));
+            }, 0);
+
+        $grandTotal = $subTotal + floatval($this->tax_amount) - floatval($this->discount_amount)
+            + ($subTotal * floatval($this->tax_percentage))
+            - ($subTotal * floatval($this->discount_percentage));
+
+        $paymentsTotal = is_numeric($this->paymentsTotal)
+            ? floatval($this->paymentsTotal)
+            : $this->payments->reduce(function($carry, $item) {
+            return $carry + floatval($item->payment_amount) - floatval($item->refund_amount);
+        }, 0);
+
         return [
             'order_num'          => $this->order_num,
             'createdAt'          => $this->created_at,
-            'date'               => $this->order_date,
+            'date'               => $this->order_date->toDateString(),
             'discountAmount'     => $this->discount_amount,
             'discountPercentage' => $this->discount_percentage,
-            'customer'           => new CustomerResource($this->whenLoaded('customer')),
-            $this->mergeWhen($this->whenLoaded('contents'), function () {
+            $this->mergeWhen($this->whenLoaded('customer'), [
+                'customer'           => new CustomerResource($this->customer),
+                'customerName'       => $this->customer->name,
+            ]),
+            $this->mergeWhen($this->whenLoaded('contents'), function () use ($subTotal, $grandTotal) {
 
-                    $subTotal = is_numeric($this->subTotal)
-                        ? floatval($this->subTotal)
-                        : $this->contents->reduce(function($carry, $item) {
-                        return $carry + (intval($item->qty) * floatval($item->unit_price));
-                    }, 0);
+
                     return [
                         'contents'           => OrderContentResource::collection($this->contents),
                         'subTotal'   => $subTotal,
-                        'grandTotal' => $subTotal + floatval($this->tax_amount) - floatval($this->discount_amount)
-                            + ($subTotal * floatval($this->tax_percentage)) - ($subTotal * floatval($this->discount_percentage))
+                        'grandTotal' => $grandTotal,
                     ];
                 }
             ),
             $this->mergeWhen($this->whenLoaded('payments'), [
                 'payments'           => PaymentResource::collection($this->payments),
-                'paymentsTotal'  => is_numeric($this->paymentsTotal)
-                    ? floatval($this->paymentsTotal)
-                    : $this->payments->reduce(function($carry, $item) {
-                    return $carry + floatval($item->payment_amount) - floatval($item->refund_amount);
-                }, 0),
+                'paymentsTotal'  => $paymentsTotal,
             ]),
+
+            'balance' => $this->when($this->whenLoaded('payments') && $this->whenLoaded('payments'),
+                $grandTotal - $paymentsTotal),
+
             'returns'            => OrderReturnResource::collection($this->whenLoaded('returns')),
             'taxAmount'          => $this->tax_amount,
             'taxPercentage'      => $this->tax_percentage,
--- resources/js/Pages/Orders.vue
+++ resources/js/Pages/Orders.vue
@@ -1,34 +1,68 @@
 <template>
-    <base-table :headers="headers" :rows="data" />
+    <div class="h-full flex">
+        <navigation :items="menu"/>
+        <div class="h-screen max-w-full flex flex-wrap p-10 overflow-scroll">
+            <div class="w-1/2">
+                <h1 class="text-2xl font-extrabold text-gray-600">{{ title }}</h1>
+                <h2 class="text-sm text-gray-500 mb-8">All our customers</h2>
+            </div>
+            <div class="w-1/2">
+                <search-bar />
+            </div>
+            <base-table
+                :class="'bg-white'"
+                :labels="labels"
+                :rows="orders.data"
+                :textRight="textRight"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
 
 import BaseTable from '@/Components/BaseTable';
+import Navigation from "@/Components/Navigation";
+import SearchBar from "@/Components/SearchBar";
 
 export default {
-    components: {BaseTable},
+    components: {
+        BaseTable,
+        Navigation,
+        SearchBar,
+    },
     props: {
         orders : {
             type: Object,
             default: {}
-        }
-    },
-
-    computed : {
-
-        data() {
-            return this.orders.data ?? false;
         },
 
-        headers(){
-            if (this.data.length) {
-                return Object.keys(this.data[0]);
-            }
+        menu:{
+            type: Object,
+            default: {}
+        },
 
-            return [];
+        title: {
+            type: String,
+            default: 'Orders',
+        },
+    },
+    data() {
+        return {
+            labels: {
+                order_num: 'Order #',
+                date: 'Order Date',
+                customerName: 'Customer',
+                grandTotal: 'Total',
+                paymentsTotal: 'Total Payments',
+                balance: 'Balance',
+            },
+
+            textRight: [
+                'grandTotal', 'paymentsTotal', 'balance',
+            ],
         }
-    }
+    },
 
 }
 </script>
