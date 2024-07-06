328f85476378b636c2bb0eb612a49fd64762639d
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2024-07-06 01:19:38 -0400

    Refactor controllers

---
+++ controllers/controller.go
@@ -0,0 +1,331 @@
+package controllers
+
+import (
+	"cross-country/models"
+	"cross-country/utilities"
+	"fmt"
+	"math"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func respond(c *gin.Context, data map[string]any) {
+	acceptHeader := c.Request.Header.Get("Accept")
+
+	if strings.Contains(acceptHeader, "application/json") {
+		c.JSON(http.StatusOK, data)
+	} else {
+		c.HTML(http.StatusOK, "home", gin.H{"data": data})
+	}
+}
+
+func getParams(c *gin.Context) (url.Values, string, string, string, string) {
+	return c.Request.URL.Query(),
+		c.DefaultQuery("perPage", "10"),
+		c.DefaultQuery("page", "1"),
+		c.DefaultQuery("orderBy", "created_at"),
+		c.DefaultQuery("order", "DESC")
+}
+
+func getPaginationLinks(totalPages, page int, url string, filters utilities.Filters) []utilities.PaginationLink {
+	links := []utilities.PaginationLink{}
+	for i := 1; i <= totalPages; i++ {
+		link := url + strconv.Itoa(i)
+		for k, v := range filters {
+			link = link + "&" + k + "=" + v
+		}
+		links = append(links, utilities.PaginationLink{
+			Label:         strconv.Itoa(i),
+			Link:          link,
+			IsCurrentPage: i == page,
+		})
+	}
+
+	return links
+}
+
+func ConsignmentIndex(c *gin.Context) {
+	params, perPageParam, pageParam, orderByParam, orderParam := getParams(c)
+	var err error
+	var perPage, page, skip, total int
+	var cArr []models.Consignment
+	var consignment models.Consignment
+
+	url := c.Request.URL.Path + fmt.Sprintf("?order=%s&orderBy=%s&perPage=%s&page=", orderParam, orderByParam, perPageParam)
+	filters := utilities.GetFilters(c, params)
+
+	perPage, err = strconv.Atoi(perPageParam)
+
+	if err == nil {
+		page, err = strconv.Atoi(pageParam)
+		skip = (page - 1) * perPage
+	}
+
+	if err == nil {
+		cArr, err = consignment.Get(filters.ToSql(), orderParam, orderByParam, skip, perPage)
+	}
+
+	if err == nil {
+		total, err = consignment.Count(filters.ToSql())
+	}
+
+	totalPages := int(math.Ceil(float64(total) / float64(perPage)))
+	links := getPaginationLinks(totalPages, page, url, filters)
+
+	results := utilities.PaginatedResults[models.Consignment]{
+		Items:      cArr,
+		Filters:    filters,
+		PerPage:    perPage,
+		TotalPages: totalPages,
+		Page:       page,
+		Total:      total,
+		Links:      links,
+		OrderBy:    orderByParam,
+		Order:      orderParam,
+	}
+
+	if err == nil {
+		respond(c, map[string]any{
+			"err":          nil,
+			"consignments": results,
+		})
+
+		return
+	}
+
+	respond(c, map[string]any{
+		"err":          fmt.Sprintf("%v", err),
+		"consignments": results,
+	})
+}
+
+func ContainerIndex(c *gin.Context) {
+	params, perPageParam, pageParam, orderByParam, orderParam := getParams(c)
+	var err error
+	var perPage, page, skip, total int
+	var cArr []models.Container
+	var container models.Container
+
+	url := c.Request.URL.Path + fmt.Sprintf("?order=%s&orderBy=%s&perPage=%s&page=", orderParam, orderByParam, perPageParam)
+	filters := utilities.GetFilters(c, params)
+
+	perPage, err = strconv.Atoi(perPageParam)
+
+	if err == nil {
+		page, err = strconv.Atoi(pageParam)
+		skip = (page - 1) * perPage
+	}
+
+	if err == nil {
+		cArr, err = container.Get(filters.ToSql(), orderParam, orderByParam, skip, perPage)
+	}
+
+	if err == nil {
+		total, err = container.Count(filters.ToSql())
+	}
+
+	totalPages := int(math.Ceil(float64(total) / float64(perPage)))
+	links := getPaginationLinks(totalPages, page, url, filters)
+
+	results := utilities.PaginatedResults[models.Container]{
+		Items:      cArr,
+		Filters:    filters,
+		PerPage:    perPage,
+		TotalPages: totalPages,
+		Page:       page,
+		Total:      total,
+		Links:      links,
+		OrderBy:    orderByParam,
+		Order:      orderParam,
+	}
+
+	if err == nil {
+		respond(c, map[string]any{
+			"err":        nil,
+			"containers": results,
+		})
+
+		return
+	}
+
+	respond(c, map[string]any{
+		"err":        fmt.Sprintf("%v", err),
+		"containers": results,
+	})
+}
+
+func CustomerIndex(c *gin.Context) {
+	params := c.Request.URL.Query()
+	perPageParam := c.DefaultQuery("perPage", "10")
+	pageParam := c.DefaultQuery("page", "1")
+	orderByParam := c.DefaultQuery("orderBy", "id")
+	orderParam := c.DefaultQuery("order", "DESC")
+	var err error
+	var perPage, page, skip, total int
+	var cArr []models.Customer
+	var customer models.Customer
+
+	url := c.Request.URL.Path + fmt.Sprintf("?order=%s&orderBy=%s&perPage=%s&page=", orderParam, orderByParam, perPageParam)
+	filters := utilities.GetFilters(c, params)
+
+	perPage, err = strconv.Atoi(perPageParam)
+
+	if err == nil {
+		page, err = strconv.Atoi(pageParam)
+		skip = (page - 1) * perPage
+	}
+
+	if err == nil {
+		cArr, err = customer.Get(filters.ToSql(), orderParam, orderByParam, skip, perPage)
+	}
+
+	fmt.Println(err)
+	if err == nil {
+		total, err = customer.Count(filters.ToSql())
+	} else {
+		fmt.Println("err not nil")
+	}
+
+	totalPages := int(math.Ceil(float64(total) / float64(perPage)))
+	links := getPaginationLinks(totalPages, page, url, filters)
+
+	results := utilities.PaginatedResults[models.Customer]{
+		Items:      cArr,
+		Filters:    filters,
+		PerPage:    perPage,
+		TotalPages: totalPages,
+		Page:       page,
+		Total:      total,
+		Links:      links,
+		OrderBy:    orderByParam,
+		Order:      orderParam,
+	}
+
+	if err == nil {
+		respond(c, map[string]any{
+			"err":       nil,
+			"customers": results,
+		})
+
+		return
+	}
+
+	respond(c, map[string]any{
+		"err":       fmt.Sprintf("%v", err),
+		"customers": results,
+	})
+}
+
+func LcIndex(c *gin.Context) {
+	params, perPageParam, pageParam, orderByParam, orderParam := getParams(c)
+	var err error
+	var perPage, page, skip, total int
+	var lcArr []models.Lc
+	var lc models.Lc
+
+	url := c.Request.URL.Path + fmt.Sprintf("?order=%s&orderBy=%s&perPage=%s&page=", orderParam, orderByParam, perPageParam)
+	filters := utilities.GetFilters(c, params)
+
+	perPage, err = strconv.Atoi(perPageParam)
+
+	if err == nil {
+		page, err = strconv.Atoi(pageParam)
+		skip = (page - 1) * perPage
+	}
+
+	if err == nil {
+		lcArr, err = lc.Get(filters.ToSql(), orderParam, orderByParam, skip, perPage)
+	}
+
+	if err == nil {
+		total, err = lc.Count(filters.ToSql())
+	}
+
+	totalPages := int(math.Ceil(float64(total) / float64(perPage)))
+	links := getPaginationLinks(totalPages, page, url, filters)
+
+	results := utilities.PaginatedResults[models.Lc]{
+		Items:      lcArr,
+		Filters:    filters,
+		PerPage:    perPage,
+		TotalPages: totalPages,
+		Page:       page,
+		Total:      total,
+		Links:      links,
+		OrderBy:    orderByParam,
+		Order:      orderParam,
+	}
+
+	if err == nil {
+		respond(c, map[string]any{
+			"err": nil,
+			"lcs": results,
+		})
+
+		return
+	}
+
+	respond(c, map[string]any{
+		"err": fmt.Sprintf("%v", err),
+		"lcs": results,
+	})
+}
+
+func TyreIndex(c *gin.Context) {
+	params, perPageParam, pageParam, orderByParam, orderParam := getParams(c)
+	var err error
+	var perPage, page, skip, total int
+	var tArr []models.Tyre
+	var container models.Tyre
+
+	url := c.Request.URL.Path + fmt.Sprintf("?order=%s&orderBy=%s&perPage=%s&page=", orderParam, orderByParam, perPageParam)
+	filters := utilities.GetFilters(c, params)
+
+	perPage, err = strconv.Atoi(perPageParam)
+
+	if err == nil {
+		page, err = strconv.Atoi(pageParam)
+		skip = (page - 1) * perPage
+	}
+
+	if err == nil {
+		tArr, err = container.Get(filters.ToSql(), orderParam, orderByParam, skip, perPage)
+	}
+
+	if err == nil {
+		total, err = container.Count(filters.ToSql())
+	}
+
+	totalPages := int(math.Ceil(float64(total) / float64(perPage)))
+	links := getPaginationLinks(totalPages, page, url, filters)
+
+	results := utilities.PaginatedResults[models.Tyre]{
+		Items:      tArr,
+		Filters:    filters,
+		PerPage:    perPage,
+		TotalPages: totalPages,
+		Page:       page,
+		Total:      total,
+		Links:      links,
+		OrderBy:    orderByParam,
+		Order:      orderParam,
+	}
+
+	if err == nil {
+		respond(c, map[string]any{
+			"err":   nil,
+			"tyres": results,
+		})
+
+		return
+	}
+
+	respond(c, map[string]any{
+		"err":   fmt.Sprintf("%v", err),
+		"tyres": results,
+	})
+}
